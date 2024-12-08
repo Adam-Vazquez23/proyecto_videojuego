320125d6d42100a828fcf342b85ea4c1c17b1d79
Adam <Adamv895@gmail.com>
2024-12-08 16:12:53 -0600

    Login exitoso :)

--- videojuegos_project/videojuegos/urls.py
+++ videojuegos_project/videojuegos/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('crear/', views.crear_videojuego, name='crear_videojuego'),
     path('actualizar/<int:id>/', views.actualizar_videojuego, name='actualizar_videojuego'),
     path('borrar/<int:id>/', views.borrar_videojuego, name='borrar_videojuego'),
-    path('listar/', views.listar_videojuegos, name='listar_videojuegos'),
+    path('listar', views.listar_videojuegos, name='listar_videojuegos'),
+    path('registro/', views.registro, name='registro'),
+    path('', views.inicio_sesion, name='login'),
+    path('logout/', views.cerrar_sesion, name='logout'),
+
 ]
--- videojuegos_project/videojuegos_project/settings.py
+++ videojuegos_project/videojuegos_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'videojuegos_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
